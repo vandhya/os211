849d4eb972b5c03b3714b2dca115361b1686eb8f
Arllivandhya Dani <vandhya12@gmail.com>
2021-03-17 08:39:41 +0700

    OS211 vandhya

--- TXT/myscript.sh
+++ TXT/myscript.sh
@@ -16,7 +16,7 @@
 # You new to set "REC2" with your own Public-Key Identity!
 # Check it out with "gpg --list-key"
 
-REC2="cbk@dummy"
+REC2="06D5A73F697F6192952DF37F4F3CE9C68A69D0F8"
 REC1="operatingsystems@vlsm.org"
 FILES="my*.asc my*.txt my*.sh"
 SHA="SHA256SUM"
